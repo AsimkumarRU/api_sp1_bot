38d1468d7a832a95216b198f30844c497d9fcc8a
Алексей Перерва <a.p.pererva@gmail.com>
2021-03-22 19:21:08 +0300

    sprint8 API 22.03.21 v.2.0

--- homework.py
+++ homework.py
@@ -13,28 +13,38 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    good_job = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    if homework.get('status') != 'approved':
+    if homework.get('status') == 'reviewing':
+        verdict = 'Работа взята в ревью.'
+    elif homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
+    elif homework.get('status') == 'approved':
+        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему ' \
+                  'уроку.'
     else:
-        verdict = good_job
+        verdict = 'Неверный ответ сервера'
+        logging.error('Неверный ответ сервера')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    data = {'from_date': current_timestamp}
+    data = current_timestamp or int(time.time())
     try:
-        homework_statuses = requests.get(url, params=data, headers=headers)
+        homework_statuses = requests.get(
+            url,
+            params={'from_date': data}, headers=headers)
         return homework_statuses.json()
-    except requests.exceptions.RequestException as e:
-        logging.exception(f'Обнаружена ошибка подключения. Error: {e}')
-    except ValueError as e:
-        logging.exception(f'Обнаружена ошибка значения. Error: {e}')
+    except (requests.exceptions.RequestException, ValueError) as e:
+        logging.exception(f'Обнаружена ошибка. Error: {e}')
     return {}
 
 
@@ -58,7 +68,7 @@ def main():
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+            logging.exception(f'Бот столкнулся с ошибкой: {e}')
             time.sleep(5)
 
 
